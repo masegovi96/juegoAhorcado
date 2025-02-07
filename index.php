169fb83d86c429f7fd9090f15d69cc499d73cf2c
ALE\alexd <alex.diaz0112@outlook.com>
2025-02-07 16:22:22 -0600

    -Se crea la página de inicio index.php -Se crea el formulario de registro -Estilos iniciales

---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="index.css">
+  <title>Ahorcado</title>
+  <style>
+
+  </style>
+</head>
+<body>
+
+  <div class="container">
+    <h1>Bienvenido al Ahorcado</h1>
+    
+    <!-- Formulario de Inicio de Sesion -->
+    <div id="login">
+      <h2>Iniciar Sesion</h2>
+      <form>
+        <input type="text" placeholder="Usuario" id="Usuario" required><br>
+        <input type="password" placeholder="Contraseña" id="Contrasena" required><br>
+        <button type="submit">Entrar</button>
+      </form>
+      <div class="links">
+        <a href="registro.html">Crear Cuenta</a>
+      </div>
+    </div>
+</body>
+</html>
---
+++ registro.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="registro.css">
+    <title>Document</title>
+</head>
+<body>
+    <div class="container">
+        <div id="register">
+            <h2>Registrarse</h2>
+            <form>
+            <input type="text" placeholder="Usuario" id="registrar-Usuario" required><br>
+            <input type="password" placeholder="Contraseña" id="registrar-Contrasena" required><br>
+            <input type="password" placeholder="Confirmar Contraseña" id="confirmar-Contrasena" required><br>
+            <button type="submit">Registrarse</button>
+            </form>
+            <div class="links">
+            <a href="index.html">Ya tengo cuenta</a>
+            </div>
+        </div>
+        </div>
+    </div>
+</body>
+</html>
